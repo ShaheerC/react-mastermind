7ec47765fa7d33187c07ab2ad79ac97a658fa2db
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-04 15:56:08 -0400

    caught up to freds commits made during class

--- src/Pages/Gamepages/gamepage.js
+++ src/Pages/Gamepages/gamepage.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import GameBoard from '../../components/GameBoard/GameBoard';
+import ColorPicker from '../../components/ColorPicker/ColorPicker';
+import GameTimer from '../../components/GameTimer/GameTimer';
+import NewGameButton from '../../components/NewGameButton/NewGameButton';
+import './gamepage.css';
 
-const GamePage = (props) => {
+const GamePage = ({
+    winTries,
+    colors,
+    guesses,
+    handlePegClick,
+    handleScoreClick,
+    selColorIdx,
+    handleColorSelection,
+    handleNewGameClick
+  }) => {
+
     return (
         <>
-        
+            <div className="flex-h align-flex-end">
+                <GameBoard
+                    colors={colors}
+                    guesses={guesses}
+                    handlePegClick={handlePegClick}
+                    handleScoreClick={handleScoreClick}
+                />
+                <div className='App-controls'>
+                    <ColorPicker
+                        colors={colors}
+                        selColorIdx={selColorIdx}
+                        handleColorSelection={handleColorSelection}
+                    />
+                    <GameTimer />
+                    <Link className="btn btn-default GamePage-link-margin" to="/settings">Difficulty</Link>
+                    <NewGameButton handleNewGameClick={handleNewGameClick}/>
+                </div>
+            </div>
+            <footer className='App-header-footer'>
+                {(winTries ? `You Won in ${winTries} Guesses!` : 'Good Luck!')}
+            </footer>
         </>
     )
 }
